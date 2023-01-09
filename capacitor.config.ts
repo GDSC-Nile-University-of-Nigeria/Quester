dcf4881b5ceff7c576882eecd006e301043297e8
fortune710 <fortune0208@yahoo.com>
2023-01-09 15:21:13 +0100

    capacitor config

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.quester',
+  appName: 'quester',
+  webDir: 'public',
+  bundledWebRuntime: false
+};
+
+export default config;
