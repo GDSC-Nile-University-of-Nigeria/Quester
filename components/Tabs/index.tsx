6df8f311e5475e7648c0e38e2c951747888e14d3
fortune710 <fortune0208@yahoo.com>
2023-02-11 03:56:37 +0100

    created some components

---
+++ components/Tabs/index.tsx
@@ -0,0 +1,55 @@
+import { MdHome, MdDownload, MdAdd, MdMenu } from "react-icons/md";
+import { FaUser } from "react-icons/fa";
+import styles from "./style.module.scss";
+import { useRouter } from "next/router";
+import Link from "next/link";
+
+export const Tabs: React.FC = () => {
+    const router = useRouter()
+    
+
+    return(
+        <footer className={styles.footer}>
+            <div className={styles.container}>
+
+                <Link href={'/home'} className={styles.tab} title="Home">
+                   <MdHome 
+                        size={25}
+                        className={`${router.pathname === '/home' ? styles.active: styles.inactive}`} 
+                    />
+                </Link>
+
+                <span className={styles.tab} title="Downloads">
+                    <MdDownload 
+                        size={25}
+                        className={`${router.pathname === '/downloads' ? styles.active: null}`}
+                    />
+                </span>
+
+                <span className={styles.tab}>
+                    <button 
+                        className={styles.addButton} 
+                        title="Add"
+                        onClick={() => router.push('/add')}
+                    >
+                        <MdAdd size={25} />
+                    </button>
+                </span>
+
+                <span className={styles.tab}>
+                    <FaUser 
+                        size={25}
+                        className={`${router.pathname === '/profile' ? styles.active: null}`}
+                    />
+                </span>
+
+                <span className={styles.tab}>
+                    <MdMenu 
+                        size={25}
+                        className={`${router.pathname === '/menu' ? styles.active: null}`}
+                    />
+                </span>
+            </div>
+        </footer>
+    )
+}
