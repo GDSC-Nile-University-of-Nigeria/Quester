da0e31d7f298bdce60986a42d6e143cc02784364
fortune710 <fortune0208@yahoo.com>
2023-02-11 03:53:15 +0100

    added custom elements loader for pwa elements

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,14 @@
-import '../styles/globals.css'
+import { useEffect } from 'react';
+import '../styles/globals.css';
+import { defineCustomElements } from '@ionic/pwa-elements/loader';
 
 function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    defineCustomElements(window)
+
+  }, [])
   return <Component {...pageProps} />
 }
+
 
 export default MyApp
