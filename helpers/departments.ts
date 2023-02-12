57169a386c1a06f9a9230bf2f17152956aaebe45
fortune710 <fortune0208@yahoo.com>
2023-02-12 23:55:56 +0100

    helper functions and arrays

---
+++ helpers/departments.ts
@@ -0,0 +1,140 @@
+import { Department } from "../types";
+
+export const DEPARTMENTS: Department[] = [
+    {
+        id: 1,
+        name: "Accounting"
+    },
+    {
+        id: 2,
+        name: "Architecture"
+    },
+    {
+        id: 3,
+        name: "Banking and Finance"
+    }, 
+    {
+        id: 4,
+        name: "Computer Science"
+    },
+    {
+        id: 5,
+        name: "Computer Engineering"
+    },
+    { 
+        id: 6,
+        name: "Civil Engineering"
+    },
+    {
+        id: 7,
+        name: "Criminology & Security Studies"
+    },
+    {
+        id: 8,
+        name: "English Language & Communication Studies"
+    },
+    {
+        id: 9,
+        name: "Economics"
+    },
+    {
+        id: 10,
+        name: "Biotechnology"
+    },
+    {
+        id: 11,
+        name: "Biochemistry"
+    },
+    {
+        id: 12,
+        name: "Business Administration"
+    },
+    {
+        id: 13,
+        name: "Cybersecurity"
+    },
+    {
+        id: 14,
+        name: "Estate Management"
+    },
+    {
+        id: 15,
+        name: "Human Anatomy"
+    },
+    {
+        id: 16,
+        name: "Human Physiology"
+    },
+    {
+        id: 17,
+        name: "Information Technology"
+    },
+    {
+        id: 18,
+        name: "Marketing"
+    },
+    {
+        id: 19,
+        name: "Mass Communication"
+    },
+    {
+        id: 20,
+        name: "Microbiology"
+    },
+    {
+        id: 21,
+        name: "Political Science"
+    },
+    {
+        id: 22,
+        name: "Psychology"
+    },
+    {
+        id: 23,
+        name: "Public Administration"
+    },
+    {
+        id: 24,
+        name: "Quantity Surveying"
+    },
+    {
+        id: 25,
+        name: "Sociology"
+    },
+    {
+        id: 26,
+        name: "Software Engineering"
+    },
+    {
+        id: 27,
+        name: "Law"
+    },
+    {
+        id: 28,
+        name: "Medicine"
+    },
+    {
+        id: 29,
+        name: "Petroluem and Gas Engineering"
+    },
+    {
+        id: 30,
+        name: "Mechanical Engineering"
+    },
+    {
+        id: 31,
+        name: "Mechatronics Engineering"
+    },
+    {
+        id: 32,
+        name: "Electrical and Electronics Engineering"
+    },
+    {
+        id: 33,
+        name: "Chemical Engineering"
+    },
+    {
+        id: 34,
+        name: "Entrepreneurship"
+    }
+]
