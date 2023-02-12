80ee1b2e93fa952beaa32586e474979482b3199d
fortune710 <fortune0208@yahoo.com>
2023-02-13 00:00:43 +0100

    changes

--- pages/dashboard/home.tsx
+++ pages/dashboard/home.tsx
@@ -1,14 +1,35 @@
 import { NextPage } from "next";
 import { Tabs } from "../../components/Tabs";
-import { auth } from "../../environments/firebase.utils";
+import { auth, firestore } from "../../environments/firebase.utils";
+import { InputBase } from "@mui/material";
+import { FaSearch } from "react-icons/fa";
+import styles from "../../styles/Home.module.scss"
+import { useEffect, useState } from "react";
+import { useDebouce } from "../../hooks/useDebouce";
 
 const HomePage: NextPage = () => {
     const { currentUser } = auth;
+    const [queryString, setQueryString] = useState("");
+    const { debouncedValue } = useDebouce(queryString);
+
+    useEffect(() => {
+        console.log(debouncedValue)
+    }, [queryString])
 
     return(
         <main>
-            <h1>Welcome, {currentUser?.displayName}</h1>
-
+            <h1 className={styles.Greeting}>
+                Welcome, {currentUser?.displayName}
+            </h1>
+            
+            <div className={styles.Searchbar}>
+                <FaSearch/>
+                <InputBase 
+                    onChange={(e) => setQueryString(e.target.value)}
+                    placeholder="Search for a past question" 
+                    type="search"
+                />
+            </div>
             <div>
                 <h3>Recents</h3>
                 
